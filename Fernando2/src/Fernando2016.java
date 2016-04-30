8551cec8005e60654282bb685fbe3859e277ccfe
Tschuss <jiglesgom@gmail.com>
2016-04-30 17:01:00 +0200

    .

--- Fernando2/src/Fernando2016.java
+++ Fernando2/src/Fernando2016.java
@@ -19,7 +19,11 @@ public class Fernando2016 {
 		XSSFWorkbook book = null;
 		FileOutputStream fos = null;
 		try {
-			File excel = new File (args[0]);
+			File excel = new File ("D:/Downloads/TRABAJO (1).xlsx");
+			int header_col= 3; // la D
+			int data_col=6; //G
+			int last_row=43;
+			
 			fis = new FileInputStream(excel); 
 			book = new XSSFWorkbook(fis); 
 			XSSFSheet sheet = book.getSheetAt(0); 
@@ -31,21 +35,21 @@ public class Fernando2016 {
 			XSSFCell cell2=null;
 			
 			//copiamos los conceptos como cabecera
-			String[] conceptos=new String[19];
-			String[] conceptos2=new String[19];
+			String[] conceptos=new String[last_row-23];
+			String[] conceptos2=new String[last_row-23];
 			int j=0;
 			int k=0;
-			for (int i=2; i<42 ;i++){
+			for (int i=2; i<last_row ;i++){
 				if (i==16 || i==17 || i==23 ||i==30 || i==33 || i==36 || i==39) continue;
 				System.out.println("Siguente concepto:" +i);
 				String header=null;
 				if (i<16) {
-					header=sheet.getRow(i).getCell(1).getStringCellValue();
+					header=sheet.getRow(i).getCell(header_col).getStringCellValue();
 					cell2=getOrCreateCell(row2, j++);
 					cell2.setCellValue(header);
 				} else {
-					conceptos[k]=sheet.getRow(i).getCell(0).getStringCellValue();
-					conceptos2[k++]=sheet.getRow(i).getCell(1).getStringCellValue();
+					conceptos[k]=sheet.getRow(i).getCell(header_col-1).getStringCellValue();
+					conceptos2[k++]=sheet.getRow(i).getCell(header_col).getStringCellValue();
 				}
 			}
 			//completamos con dos cabeceras más: concepto y puntos
@@ -63,43 +67,46 @@ public class Fernando2016 {
 			
 			
 			//copiamos los datos de los informes periciales
-			k=5;
+			k=data_col;
 			String[] report_data= new String[14];
-			Double[] report_points= new Double[19];
+			Double[] report_points= new Double[last_row-23];
 			
 			while (sheet.getRow(2).getCell(k) != null) {
 				j=0;
-				for (int i=2; i<16 ;i++){
-					//guardarmos los campos a repetir
-
-					switch (sheet.getRow(i).getCell(k).getCellType()) { 
-					case Cell.CELL_TYPE_STRING: report_data[j++]=sheet.getRow(i).getCell(k).getStringCellValue(); break;
-					case Cell.CELL_TYPE_NUMERIC: report_data[j++]=""+Math.round(sheet.getRow(i).getCell(k).getNumericCellValue()); break;
-					default: 
+				try {
+					for (int i=2; i<16 ;i++){
+						//guardarmos los campos a repetir
+	
+						switch (sheet.getRow(i).getCell(k).getCellType()) { 
+						case Cell.CELL_TYPE_STRING: report_data[j++]=sheet.getRow(i).getCell(k).getStringCellValue(); break;
+						case Cell.CELL_TYPE_NUMERIC: report_data[j++]=""+Math.round(sheet.getRow(i).getCell(k).getNumericCellValue()); break;
+						default: 
+						}
 					}
-				}
-				System.out.println(Arrays.asList(report_data));
-
-				j=0;
-				for (int i=17; i<42 ;i++){
-					//y guardamos las puntuaciones
-					if (i==16 || i==17 || i==23 ||i==30 || i==33 || i==36 || i==39) continue;
-					report_points[j++]=sheet.getRow(i).getCell(k).getNumericCellValue();
-				}
-				System.out.println(Arrays.asList(report_points));
-				
-				//escribimos los campos a repetir, por cada puntuacion obtenida
-				for (int a=0;a<report_points.length; a++) {
-					row2=getOrCreateRow(sheet2, row2count++);
-					for (int b=0;b<report_data.length;b++) {
-						getOrCreateCell(row2, b).setCellValue(report_data[b]);
+					System.out.println(Arrays.asList(report_data));
+	
+					j=0;
+					for (int i=17; i<last_row ;i++){
+						//y guardamos las puntuaciones
+						if (i==16 || i==17 || i==23 ||i==30 || i==33 || i==36 || i==39) continue;
+						report_points[j++]=sheet.getRow(i).getCell(k).getNumericCellValue();
 					}
-					getOrCreateCell(row2, report_data.length).setCellValue(conceptos[a]);
-					getOrCreateCell(row2, report_data.length+1).setCellValue(conceptos2[a]);
-					getOrCreateCell(row2, report_data.length+2).setCellValue(report_points[a]);
+					System.out.println(Arrays.asList(report_points));
 					
+					//escribimos los campos a repetir, por cada puntuacion obtenida
+					for (int a=0;a<report_points.length; a++) {
+						row2=getOrCreateRow(sheet2, row2count++);
+						for (int b=0;b<report_data.length;b++) {
+							getOrCreateCell(row2, b).setCellValue(report_data[b]);
+						}
+						getOrCreateCell(row2, report_data.length).setCellValue(conceptos[a]);
+						getOrCreateCell(row2, report_data.length+1).setCellValue(conceptos2[a]);
+						getOrCreateCell(row2, report_data.length+2).setCellValue(report_points[a]);
+						
+					}
+				} catch (IllegalStateException e) {
+					System.err.println(e);
 				}
-				
 				k++;
 			}			
 			
